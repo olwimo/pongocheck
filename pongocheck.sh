41b3ffd4a7e97f231acd9fcb7a7763c513729103
Morten Winther Olsson <18525061+olwimo@users.noreply.github.com>
2022-05-11 23:00:44 +0200

    Update pongocheck.sh

--- pongocheck.sh
+++ pongocheck.sh
@@ -35,15 +35,21 @@ fi
 
 if ! type openssl &>/dev/null; then if ! type brew &>/dev/null; then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; fi ; brew install openssl ; fi
 
+dir="$(pwd)"
+cd /tmp
+
 uuid="$(uuidgen)"
-mkdir -p /tmp/"$uuid"
-cd /tmp/"$uuid"
+mkdir "$uuid"
+cd "$uuid"
 
 curl -fsSL "https://github.com/olwimo/pongocheck/raw/main/pongocheck.tar.gz.enc" | openssl enc -aes-256-cbc -d | tar xz
 
 cd pongocheck
 ./pongoterm <cmd.txt &
 ./checkra1n.app/Contents/MacOS/checkra1n -k Pongo.bin -g
-cd /tmp
+cd ..
 
+cd ..
 rm -rf "$uuid"
+
+cd "$dir"
